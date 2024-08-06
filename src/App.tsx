757f989a595fafad8c0fc284b6dfc9c795ab65f2
sudhy44 <66510639+sudhy44@users.noreply.github.com>
2024-08-06 11:03:28 -0400

    Model added

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { ChatUI } from "./components/chat-ui/ChatUI";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMailReply } from "@fortawesome/free-solid-svg-icons";
 
+import { HfInference } from "@huggingface/inference";
 const TEST_USER_INFO = { firstName: "Test", lastName: "User" };
 function App() {
   const [isQuerying, setIsQuerying] = useState<boolean>(false);
@@ -18,6 +19,57 @@ function App() {
     }
   ]);
 
+
+ 
+  
+  // const hf = new HfInference('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');
+
+  // async function query(data: any) {
+  //   console.log("bit", data)
+  //   const response = await fetch(
+  //     "https://api-inference.huggingface.co/models/microsoft/DialoGPT-large/v1/chat/completions",
+  //     {
+  //       headers: { Authorization: "Bearer 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" },
+  //       method: "POST",
+  //       body: JSON.stringify(data),
+  //     }
+  //   );
+  //   const result = await response.json();
+  //   return result;
+  // }
+
+
+  const aifun = async (value: string) => {
+    let fullResponse = "";
+    const inference = new HfInference("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");
+
+    for await (const chunk of inference.chatCompletionStream({
+      model: "microsoft/Phi-3-mini-4k-instruct",
+      messages: [{ role: "user", content: value }],
+      max_tokens: 500,
+    })) {
+
+      // console.log(chunk.choices[0]?.delta?.content || "");
+      fullResponse += chunk.choices[0]?.delta?.content || "";
+
+      
+
+    }
+
+    console.log(fullResponse);
+    setIsQuerying(false);
+    setChatConversations((conversations) => [
+      ...conversations,
+      {
+        id: (conversations.length + 1).toString(),
+        role: MessageRole.ASSISTANT,
+        message: fullResponse,
+      },
+    ]);
+    return fullResponse;
+  }
+
+
   const handleSubmit = useCallback((value: string) => {
     setIsQuerying(true);
     setChatConversations((conversations) => [
@@ -29,17 +81,21 @@ function App() {
         message: value,
       },
     ]);
-    setTimeout(() => {
-      setIsQuerying(false);
-      setChatConversations((conversations) => [
-        ...conversations,
-        {
-          id: (conversations.length + 1).toString(),
-          role: MessageRole.ASSISTANT,
-          message: "This is a mocked sample chat bot assistant response",
-        },
-      ]);
-    }, 3000);
+
+    aifun(value);
+
+    // let test = "hgjhgdhjgasj";
+    // setTimeout(() => {
+    //   setIsQuerying(false);
+    //   setChatConversations((conversations) => [
+    //     ...conversations,
+    //     {
+    //       id: (conversations.length + 1).toString(),
+    //       role: MessageRole.ASSISTANT,
+    //       message: test,
+    //     },
+    //   ]);
+    // }, 1000);
   }, []);
 
   return (
