52b849371229384e4db8ed980202b43d441c023f
sudhy44 <66510639+sudhy44@users.noreply.github.com>
2024-08-13 01:24:44 -0400

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -98,14 +98,31 @@ function App() {
   }, []);
 
   return (
-    <ChatUI
-      isQuerying={isQuerying}
-      onSubmit={handleSubmit}
-      placeholder="Type here..."
-      disabled={isQuerying}
-      conversations={chatConversations}
-      customSubmitIcon={<FontAwesomeIcon icon={faMailReply} />}
-    />
+    <div style={{display:'flex'}}>
+      <div style={{width:'50vw'}}>
+        <ChatUI
+          isQuerying={isQuerying}
+          onSubmit={handleSubmit}
+          placeholder="Type here..."
+          disabled={isQuerying}
+          conversations={chatConversations}
+          customSubmitIcon={<FontAwesomeIcon icon={faMailReply} />}
+        />
+      </div>
+        <div style={{ width: '50vw',padding:'10px ' }}>
+        <div>
+          <iframe width="100%" height="315" src="https://www.youtube.com/embed/watIOHlOh7Y?si=UgR1mK7PTikSiSTd" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          </div>
+        <div>
+          <object data="src\pdf.pdf" type="application/pdf" width="100%" height="300px">
+            <p>Your browser doesn't support embedded PDFs.</p>
+          </object>
+          </div>
+
+        </div>
+      </div>
+    
+    
   );
 }
 
